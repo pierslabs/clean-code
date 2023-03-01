e4d0b028e1fc57b3072e08e06bd141d0bafee788
pierslabs <pedrolosasp@gmail.com>
2023-03-01 12:27:54 +0100

    Add styles.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 function App() {
   return (
     <div className="App">
-      <section style={{ display: 'flex', justifyContent: 'center', borderBottom: 'solid 1px #aaa' }}>
-        <h1 style={{ fontSize: 40, fontWeight: 'normal' }}>Clean Code</h1>
-      </section>
+      <div>
+        <h1>Clean Code</h1>
+        <div className="container">
+          <p style={{ textAlign: 'center' }}>Check console</p>
+        </div>
+      </div>
     </div>
   );
 }
