a5d7a8becfc84e880b2f7a6387b13cdc6f122601
pierslabs <pedrolosasp@gmail.com>
2023-03-01 09:01:37 +0100

    Create repo.

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App">
+      <section style={{ display: 'flex', justifyContent: 'center', borderBottom: 'solid 1px #aaa' }}>
+        <h1 style={{ fontSize: 40, fontWeight: 'normal' }}>Clean Code</h1>
+      </section>
+    </div>
+  );
+}
+
+export default App;
